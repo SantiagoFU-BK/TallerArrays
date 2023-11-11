fa903a333e4c18a20136644a6f3bb59a3004503b
SantiagoFU - BK <santiagoflorez287345@correo.itm.edu.co>
2023-11-11 00:10:06 -0500

    Finalizado, se realizan pruebas

---
+++ TallerArrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+{
+    static void Main()
+    {
+        int tamañoVector, encontrarNumero, ubicacion = 51;
+
+        Console.WriteLine("¿Cuál es el tamaño del vector?");
+        tamañoVector = int.Parse(Console.ReadLine());
+
+        int[] array = new int[tamañoVector];
+        Random random = new Random();
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = random.Next(1,50);
+        }
+
+        Console.WriteLine("¿Cuál es el número a buscar en el vector?");
+        encontrarNumero = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == encontrarNumero)
+            {
+                ubicacion = i; break;
+            }
+        }
+
+        if (ubicacion == 51)
+        {
+            Console.WriteLine($"El número {encontrarNumero} no existe en el vector");
+        }
+        else
+        {
+            Console.WriteLine($"El {encontrarNumero} se encuentra actualmente en el vector y esta en la posición {ubicacion}");
+        }
+    }
+}
